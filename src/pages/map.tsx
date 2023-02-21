a640b2ce38f4bb72da1491199a7185374f507144
Ahmad Muslih Zain <ahmad.muslih22@gmail.com>
2023-02-21 09:45:50 +0300

    add head to map page

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const MapContainer = dynamic(
   () => import("../components/map-container").then((mod) => mod.default),
@@ -10,6 +11,12 @@ const MapContainer = dynamic(
 export default function Map() {
   return (
     <div className="flex min-h-screen bg-gradient-to-br from-blue-500 to-purple-500">
+      <Head>
+        <title>Map it!</title>
+        <meta name="description" content="Map it website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <MapContainer />
     </div>
   );
